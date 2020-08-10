c4e81f7f602253a8b1ab8725a0ea6bdb5c83c418
a45hvn <sobfy01@gmail.com>
2020-08-10 15:16:22 +0900

    new workspace

---
+++ WebContent/server/WebProject/src/com/test/soccer/DBUtil.java
@@ -0,0 +1,70 @@
+package com.test.soccer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//JAVA DOC 주석
+/**
+ * 
+ * @author 아무개
+ * 오라클 연결 클래스입니다
+ * 
+ */
+public class DBUtil {
+
+	private Connection conn=null;
+	/**
+	 * 서버에 연결합니다.
+	 * @return 연결 객체를 반환합니다.
+	 */
+	public Connection open() {
+		
+//		String url="jdbc:oracle:thin:@192.168.0.1:1521:xe";//쌍용학원
+		String url="jdbc:oracle:thin:@localhost:1521:xe";//집
+		String id="hr";
+		String pw="java1234";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn=DriverManager.getConnection(url, id, pw);//접속 버튼과 동일
+			return conn;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}//open
+	
+	/**
+	 * 서버에 연결합니다.
+	 * @param host 서버 주소
+	 * @param id 계정명
+	 * @param pw 비밀번호
+	 * @return 연결 객체를 반환합니다.
+	 */
+	public Connection open(String host, String id, String pw) {
+		
+		String url="jdbc:oracle:thin:@"+host+":1521:xe";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn=DriverManager.getConnection(url, id, pw);//접속 버튼과 동일
+			return conn;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}//open
+
+	/**
+	 * 연결을 종료합니다.
+	 */
+	public void close() {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}//close
+}//DBUtil
