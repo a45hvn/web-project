298a5ade82cb1b5b94e76213105678bbcb444c5b
hejun.public <dhgmlwns1018@naver.com>
2020-08-12 18:19:01 +0900

    08/12
    게시판
    페이징, 검색 구현

--- WebContent/server/WebProject/src/com/test/soccer/board/BoardDAO.java
+++ WebContent/server/WebProject/src/com/test/soccer/board/BoardDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.test.soccer.DBUtil;
 
@@ -30,15 +31,23 @@ public class BoardDAO {
 	}
 	
 	//List 서블릿 -> 게시판 목록 주세요.
-	public ArrayList<BoardDTO> list() {
+	public ArrayList<BoardDTO> list(HashMap<String, String> map) {
 		
 		try {
 			
-			String sql = "select * from tblboard";
+			String where = "";
+		
 			
-			stat = conn.createStatement();
+			//검색어가 있을때
+			if (map.get("search") != null) {
+				 where = String.format("and title like '%%%s%%'", map.get("search"));
+			}
+			
+			String sql = String.format("select * from (select a.*, rownum as rnum from (select * from tblBoard where category_seq = 3 %s order by seq) a) where rnum >= %s and rnum <= %s", where, map.get("begin"), map.get("end"));
+		
+			stat = conn.prepareStatement(sql);			
 			rs = stat.executeQuery(sql);
-			
+						
 			ArrayList<BoardDTO> list = new ArrayList<BoardDTO>();
 			
 			//rs -> list 복사
@@ -47,14 +56,14 @@ public class BoardDAO {
 				// 레코드 1줄 -> BoardDTO 1개
 				BoardDTO dto = new BoardDTO();
 				
-				dto.setSeq(rs.getString("seq"));
-				dto.setTitle(rs.getString("title"));
-				dto.setContent(rs.getString("content"));
-				dto.setRegdate(rs.getString("regdate"));
-				dto.setReadcount(rs.getInt("readcount"));
-				dto.setReadcount(rs.getInt("category_seq"));
-				dto.setMember_seq(rs.getString("member_seq"));
-				dto.setMember_seq(rs.getString("image"));
+				dto.setSeq(rs.getString("seq"));				//번호
+				dto.setTitle(rs.getString("title"));			//제목
+				dto.setContent(rs.getString("content"));		//내용
+				dto.setRegdate(rs.getString("regdate"));		//등록일
+				dto.setReadcount(rs.getInt("readcount"));		//조회수
+				//dto.setReadcount(rs.getInt("category_seq"));	//게시판 카테고리번호
+				dto.setMember_seq(rs.getString("member_seq"));	//회원 번호
+				dto.setImage(rs.getString("image"));			//이미지
 				
 				list.add(dto);
 				
@@ -68,6 +77,36 @@ public class BoardDAO {
 		}
 		
 		return null;
+	}
+
+	//list 서블릿 -> 페이지
+	public int getTotalCount(HashMap<String, String> map) {
+				
+		try {
+			
+			String where = "";
+			
+			if(map.get("search") != null) {
+				where = String.format("and title like '%%%s%%'", map.get("search"));
+			}
+			
+			String sql = String.format("select count(*) as cnt from tblboard where category_seq = 3 %s", where);
+			
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+			
+			if(rs.next()) {
+				return rs.getInt("cnt");
+			}
+			
+		} catch (Exception e) {
+
+			System.out.println("BoardDAO.getTotalCount()");
+			e.printStackTrace();
+			
+		}
+		
+		return 0;
 	}
 	
 	
